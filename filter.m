57895bae9617c09b5707e56da27a3d1bc1e168f8
guiya.xie <569423176@qq.com>
2016-08-20 15:20:47 +0800

    修改安装包为英文版。上传滤波器参数生成代码

--- filter.m
+++ filter.m
@@ -1,37 +1,11 @@
-% fp = fopen('D:\HighPass.txt','wt');
-% format long g
-% for i =1 : 31250/2-1
-%     fprintf(fp, 'Frq=%05dHz@', i);
-%     fprintf(fp, '%d$', fir1(39,i/31250*2,'high'));
-%     fprintf(fp, '!\r');
-% end
-% fclose(fp);
-
-% fp = fopen('D:\filter_h_100000.txt','wt');
-% format long g
-% for i =1 : 12500/2-1
-%     fprintf(fp, 'Frq=%05dHz@', i);
-%     fprintf(fp, '%d$', fir1(38,i/12500*2,'high'));
-%     fprintf(fp, '!\r');
-% end
-% fclose(fp);
-% 
-% fp = fopen('D:\filter_l_100000.txt','wt');
-% format long g
-% for i =1 : 12500/2-1
-%     fprintf(fp, 'Frq=%05dHz@', i);
-%     fprintf(fp, '%d$', fir1(38,i/12500*2));
-%     fprintf(fp, '!\r');
-% end
-% fclose(fp);
-
-filterdeep = 200;
+filterdeep = 120;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %1000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_1000.txt','wt');
 format long g
+fprintf(fp, '%05d', filterdeep);
 for i =1 : 125/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
     fprintf(fp, '%d$', fir1(filterdeep,i/125*2,'high'));
@@ -40,6 +14,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_1000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 125/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -52,6 +27,7 @@ fclose(fp);
 %10000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_10000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 1250/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -61,6 +37,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_10000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 1250/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -73,6 +50,7 @@ fclose(fp);
 %20000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_20000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 2500/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -82,6 +60,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_20000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 2500/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -94,6 +73,7 @@ fclose(fp);
 %25000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_25000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 3125/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -103,6 +83,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_25000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 3125/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -115,6 +96,7 @@ fclose(fp);
 %40000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_40000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 5000/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -124,6 +106,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_40000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 5000/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -136,6 +119,7 @@ fclose(fp);
 %50000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_50000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 6250/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -145,6 +129,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_50000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 6250/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -158,6 +143,7 @@ fclose(fp);
 %100000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_100000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 12500/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -167,6 +153,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_100000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 12500/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -179,6 +166,7 @@ fclose(fp);
 %200000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_200000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 25000/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -188,6 +176,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_200000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 25000/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -200,6 +189,7 @@ fclose(fp);
 %250000采样率
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fp = fopen('D:\filter_h_250000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 31250/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -209,6 +199,7 @@ end
 fclose(fp);
 
 fp = fopen('D:\filter_l_250000.txt','wt');
+fprintf(fp, '%05d', filterdeep);
 format long g
 for i =1 : 31250/2-1
     fprintf(fp, 'Frq=%05dHz@', i);
@@ -217,4 +208,4 @@ for i =1 : 31250/2-1
 end
 fclose(fp);
 
-
+x= 1;
